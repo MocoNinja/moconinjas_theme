22d60f8f9f03e431a5e3891f3f98fced66c0e2a2
MocoNinja <javiergonzaleztello@gmail.com>
2022-12-30 15:21:49 +0100

    some tweaks

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,31 @@
 function main() {
-    console.log(
-        `DOM rendered at ${new Date().toISOString()}`
-    );
+	console.log(
+		`DOM rendered at ${nowIsoString()}`
+	);
+	bindEvents();
+	console.log(
+		`Events have been binded at ${nowIsoString()}`
+		);
 }
+
+// TODO: proper modules
+
+// events
+const stupidEvent = (event) => {
+	console.log(
+		`Event ${event} targetting ${event.target} was fired at ${nowIsoString()}`
+		);
+}
+
+// event binder
+function bindEvents() {
+	Array.from(document.getElementsByClassName('has-stupid-event'))
+		.forEach(element => {
+			element.addEventListener('click', stupidEvent);
+		});
+}
+
+// utils
+const nowIsoString = () => new Date().toISOString();
 
 window.addEventListener("load", main);
